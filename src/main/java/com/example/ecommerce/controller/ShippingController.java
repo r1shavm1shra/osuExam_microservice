021b767d43f28dddb59c27d2c527ced42b477740
Rishav <rishav_1995@yahoo.co.in>
2022-10-16 13:50:23 -0400

    Shipping Details

--- src/main/java/com/example/ecommerce/controller/ShippingController.java
+++ src/main/java/com/example/ecommerce/controller/ShippingController.java
@@ -5,22 +5,29 @@ import com.example.ecommerce.Repo.ShippingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.PublicKey;
 import java.util.List;
+import java.util.Optional;
 
+@CrossOrigin
 @RestController
 public class ShippingController {
 
     @Autowired
     private ShippingRepo shippingRepo;
 
-    @GetMapping("/users")
+    @GetMapping("/shipping")
     public List<ShippingDetails> getAll()
     {
         return shippingRepo.findAll();
     }
 
-    @PostMapping("/save")
+    @GetMapping("/shipping/{id}")
+    public Optional<ShippingDetails> getById(@PathVariable long id)
+    {
+        return shippingRepo.findById(id);
+    }
+
+    @PostMapping("/saveShipping")
     public String saveUser(@RequestBody ShippingDetails shippingDetails)
     {
         shippingRepo.save(shippingDetails);
